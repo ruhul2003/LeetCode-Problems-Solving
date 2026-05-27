1a92ff70e6493741daba944a19e770f822568914
Ruhul Amin <148174186+ruhul2003@users.noreply.github.com>
2026-05-27 22:29:04 +0600

    Implement Roman to Integer conversion function

---
+++ 13.Roman_To_int.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        unordered_map<char, int> value;
+
+        value['I'] = 1;
+        value['V'] = 5;
+        value['X'] = 10;
+        value['L'] = 50;
+        value['C'] = 100;
+        value['D'] = 500;
+        value['M'] = 1000;
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i < s.length() - 1 && value[s[i]] < value[s[i + 1]]) {
+                total -= value[s[i]];
+            }
+            else {
+                total += value[s[i]];
+            }
+        }
+
+        return total;
+    }
+};
